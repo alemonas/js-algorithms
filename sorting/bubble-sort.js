058fdfc7f7ec9aea1e0c2605b0d5d9adac52b06f
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-07-21 18:18:32 -0400

    wip: add bubble sorting algo

---
+++ sorting/bubble-sort.js
@@ -0,0 +1,22 @@
+// Bubble sort
+// Usually we shouldn't use this algorigthm in production (there are more efficient algorithms for sorting)
+// Time Complexity: Loop inside a loop -> O(n^2)
+// Spacial Complexity: Constant (because we are creating any aditional arrays, just operating over the array itself)
+// Mutate the array which is good in this case (don't worry about rules of Funcional Programming)
+
+function bubbleSort(nums) {
+  let swapped = false;
+  do {
+    swapped = false;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i + 1]) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
+
+module.exports = bubbleSort;
