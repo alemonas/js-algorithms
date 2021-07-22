73f1921d976d780c08994023d158be93154da5db
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-07-22 10:45:47 -0400

    add insertion sort algorithm exercise

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,36 @@
+/*
+  Insertion sort!
+  
+  Be sure to call your function insertionSort!
+  
+  The idea here is that the beginning of your list is sorted and the everything else is assumed to be an unsorted mess.
+  The outer loop goes over the whole list, the index of which signifies where the "sorted" part of the list is. The inner
+  loop goes over the sorted part of the list and inserts it into the correct position in the array.
+  
+  Like bubble sort, there's a visualization mechanism available to you. Just call snapshot(myArray) at the beginning of
+  your inner loop and it should handle the rest for you!
+  
+  And you put xdescribe instead of describe if you want to suspend running the unit tests.  
+*/
+
+// Is more effecient than the bubble sort algorithm, do substantially left less work than bubble sort
+// Insertion sort is more effective for collections that are mostly sorted, even better that quick sort and merge sort for mostly sorted lists
+// Time Complexity: O(n ^ 2) with better coeficients than bubble sort
+// Spacial Complexity: Constant
+
+function insertionSort(nums) {
+  for (let i = 1; i < nums.length; i++) {
+    let numberToInsert = nums[i];
+    let j;
+
+    for (j = i - 1; nums[j] > numberToInsert && j >= 0; j--) {
+      nums[j + 1] = nums[j];
+    }
+
+    nums[j + 1] = numberToInsert;
+  }
+
+  return nums;
+}
+
+module.exports = insertionSort;
